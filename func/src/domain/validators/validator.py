d82b970d507cc3081785b4f628f3aa5938ca6185
renata monteiro <rcm@lionx.com.br>
2022-07-13 10:19:45 -0300

    excluding extra dirs and adding tests dirs

--- func/src/domain/validators/validator.py
+++ func/src/domain/validators/validator.py
@@ -4,4 +4,3 @@ from pydantic import BaseModel
 
 class W8FormConfirmation(BaseModel):
     w8_confirmation: bool
-
--- func/src/infrastructure/s3/infrastructure.py
+++ func/src/infrastructure/s3/infrastructure.py
@@ -43,21 +43,3 @@ class S3Infrastructure:
         except Exception as e:
             Gladsheim.error(error=e)
             raise InternalServerError("files.error")
-
-    @classmethod
-    @asynccontextmanager
-    async def get_bucket(cls, bucket_name: str):
-        try:
-            if cls.session is None:
-                cls.session = aioboto3.Session()
-            async with cls.session.resource(
-                "s3",
-                aws_access_key_id=config("AWS_ACCESS_KEY_ID"),
-                aws_secret_access_key=config("AWS_SECRET_ACCESS_KEY"),
-                region_name=config("REGION_NAME"),
-            ) as s3:
-                bucket = await s3.Bucket(bucket_name)
-                yield bucket
-        except Exception as e:
-            Gladsheim.error(error=e)
-            raise InternalServerError("files.error")
